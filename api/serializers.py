10212a7c7a6e06e577211b01c3168fe405334356
Jayant Raj <jayantraj66041@gmail.com>
2022-02-02 22:25:52 +0530

    learn nested serializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,7 @@ from .models import Singer, Song
 
 
 class SongSerializer(serializers.ModelSerializer):
+    singer = serializers.StringRelatedField()
     class Meta:
         model = Song
         fields = ['id', 'title', 'duration', 'singer']
@@ -11,9 +12,11 @@ class SongSerializer(serializers.ModelSerializer):
 class SingerSerializer(serializers.ModelSerializer):
     #  video refrence - https://www.youtube.com/watch?v=dMJUDeKsYtI&list=PLbGui_ZYuhijTKyrlu-0g5GcP9nUp_HlN&index=33
 
-    song = serializers.StringRelatedField(many=True, read_only=True)
+    song = SongSerializer(many=True, read_only=True)    # nested api
+
+    # song = serializers.StringRelatedField(many=True, read_only=True)  # relation of song table
     # song = serializers.SlugRelatedField(many=True, read_only=True, slug_field="title")
-    
+
     class Meta:
         model = Singer
         fields = ['id', 'name', 'age', 'gender', 'song']
